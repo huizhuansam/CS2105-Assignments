8d4da7765b0a3be111301f1eb7d92b3773c7873e
Hui Zhuan <huizhuansam@hotmail.com>
2022-08-16 08:51:22 +0800

    add solution to task 2

--- Assignment_0/PacketExtr.py
+++ Assignment_0/PacketExtr.py
@@ -1,2 +1,24 @@
 import sys
 
+reader = sys.stdin.buffer
+writer = sys.stdout.buffer
+header_size = 6
+
+while True:
+    header_text = reader.read1(header_size)
+
+    if len(header_text) < header_size:
+        break
+
+    num_bytes = ''
+
+    while True:
+        digit = reader.read1(1)
+        if digit == b'B':
+            break
+        num_bytes += digit.decode('ASCII')
+
+    num_bytes = int(num_bytes)
+    data = reader.read1(num_bytes)
+    writer.write(data)
+    writer.flush()
