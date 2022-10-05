6904de7387cbb2dd9928d08b8b3a82b4497d4d37
SIEW HUI ZHUAN <huizhuan@comp.nus.edu.sg>
2022-10-06 01:04:59 +0800

    add helper functions

--- Assignment_2/Alice.py
+++ Assignment_2/Alice.py
@@ -1,1 +1,10 @@
-# blank
+import sys
+from zlib import crc32
+
+PACKET_DELIMITER = ' '.encode()
+MAXIMUM_PACKET_SIZE = 64
+
+
+def generate_packet(line: bytes) -> bytes:
+    checksum = crc32(line).to_bytes(4, sys.byteorder)
+    return checksum + PACKET_DELIMITER + line
--- Assignment_2/Bob.py
+++ Assignment_2/Bob.py
@@ -1,1 +1,34 @@
-# blank
+import sys
+from zlib import crc32
+
+
+PACKET_DELIMITER = ' '.encode()
+
+def verify_checksum(packet: bytes) -> bool:
+    packet_length = len(packet)
+    index = 0
+    packet_checksum = b''
+
+    while index < packet_length:
+        curr = packet[index].to_bytes(1, sys.byteorder)
+        index += 1
+        if curr != PACKET_DELIMITER:
+            packet_checksum += curr
+        else:
+            break
+    
+    packet_data = packet[index:]
+    checksum = crc32(packet_data).to_bytes(4, sys.byteorder)
+    return packet_checksum == checksum
+
+def extract_data(packet: bytes) -> bytes:
+    packet_length = len(packet)
+    index = 0
+
+    while index < packet_length:
+        curr = packet[index].to_bytes(1, sys.byteorder)
+        index += 1
+        if curr == PACKET_DELIMITER:
+            break
+    
+    return packet[index:]
