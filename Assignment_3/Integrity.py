d1d164760fba923fe2fde038f2f54dc9e9d9fa73
SIEW HUI ZHUAN <huizhuan@comp.nus.edu.sg>
2022-10-26 01:48:43 +0800

    add Integrity solution

--- Assignment_3/Integrity.py
+++ Assignment_3/Integrity.py
@@ -7,23 +7,25 @@ if len(sys.argv) < 3:
     print("Usage: python3 ", os.path.basename(__file__), "key_file_name document_file_name")
     sys.exit()
 
-key_file_name   = sys.argv[1]
-file_name       = sys.argv[2]
+key_file_name = sys.argv[1]
+file_name = sys.argv[2]
 
 # get the authentication key from the file
-# TODO
+KEY_NUM_BYTES = 32
+with open(key_file_name, 'rb') as a:
+    auth_key = a.read1(KEY_NUM_BYTES)
 
 # read the input file
+with open(file_name, 'rb') as a:
+    input_file = a.read()
 
 # First 32 bytes is the message authentication code
-# TODO
-mac_from_file = "get me from the file"
+mac_from_file = input_file[:KEY_NUM_BYTES]
 
 # Use the remaining file content to generate the message authentication code
-# TODO
-mac_generated = "generated from the file"
+h = SHA256.new()
+data = input_file[KEY_NUM_BYTES:]
+h.update(data + auth_key)
+mac_generated = h.digest()
 
-if mac_from_file == mac_generated:
-    print ('yes')
-else:
-    print ('no')
+print('yes') if mac_from_file == mac_generated else print('no')
